32e9f9e9146de2f7cc2e62bc583b7399a2311116
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-06-11 12:59:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     conf = {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
                   'tools.sessions.on': True}}
     cherrypy.tree.mount(DeployApp(), '/', conf)
-    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.config.update({'server.socket_host': '93.34.148.69'})
     cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', 5000))})
     cherrypy.engine.start()
     cherrypy.engine.block()
