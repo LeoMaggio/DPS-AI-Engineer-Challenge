08f35314c584558cbe64c0d705efcaf15c412a50
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-06-11 13:22:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ async def predict(item: Item):
         return {'prediction': prediction[0]}, 200
     
 if __name__ == "__main__":
-    app.run("0.0.0.0", int(os.environ.get('PORT', 5000)))
+    uvicorn.run(app, host='127.0.0.1', port=8000)
