4accc3b7d2f9e7a363377f55d81d300210d9e042
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-06-12 12:05:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 import pickle
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, flash
 
 app = Flask(__name__)
-model = pickle.load(open('./model.pkl', 'rb'))
+app.secret_key = "1234"
+model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/")
 def index():
-    output = ""
-    if request.method == "POST":
-        year = request.form["year"]
-        month = request.form["month"]
-        output = pred.prediction(year, month)     
-
-    return render_template("index.html", value=output)
-
-@app.route("/predict", methods = ["POST"])
+    flash("Prediction: ")
+    return render_template("index.html")
+    
+@app.route("/predict", methods=['POST', 'GET'])
 def predict():
-    output = ""
-    if request.method == "POST":
-        year = request.json["year"]
-        month = request.json["month"]
-        if year is None or month is None:
-            return {"error": "Missing input value"}, 400
-        elif year < 2022:
-            return {"error": "Select a year in the future"}, 400
+    year = int(request.form["year"])
+    month = int(request.form["month"])
+    target = [0, 1, year, month]
+    prediction = model.predict([target])
+    flash(f"Prediction: {prediction[0]}")
+    return render_template("index.html")
+    
+@app.route("/api/predict", methods=['POST'])
+def apiPredict():
+    if "year" not in request.get_json(force=True) or "month" not in request.get_json(force=True):
+        return {"error": "Missing input value"}, 400
+    else:
+        data = request.get_json(force=True)
+        year = data["year"]
+        month = data["month"]
+        if year < 2022:
+            return {"error": "Select a year after 2021"}, 400
         elif month < 1 or month > 12:
             return {"error": "Invalid month"}, 400
         else:
